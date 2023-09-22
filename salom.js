d56bf33774d6bd6f92509cd9d53e3a7ebd51e4a2
alijonov2184 <145776529+alijonov2184@users.noreply.github.com>
2023-09-22 18:40:28 +0500

    Add files via upload

---
+++ salom.js
@@ -0,0 +1,37 @@
+
+  gsap.to("#nom", {
+   
+    duration: 1,
+    delay: 2,
+    y: 30,
+    scale: 1,
+    
+  });
+
+
+  gsap.to("#nom1", {
+    duration: 1,
+   delay: 2,
+    y: -50,
+    scale: 1,
+  });
+
+
+var btn1 = document.getElementById("btn1")
+var demo1 = document.getElementById("demo1")
+btn1.onclick = function () {
+  let num = prompt("emailni not'g'ri yozdingiz")
+
+}
+
+// https://alijonov2184.github.io/Codext/
+
+
+
+
+
+
+
+
+
+
